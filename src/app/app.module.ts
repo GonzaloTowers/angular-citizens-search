e2b89bed3e8dbc9e55d46c3276dc2e73017a55d3
Gtowers <gonzalomg.ml@gmail.com>
2020-07-06 22:36:52 +0200

    App structure initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { CensusComponent } from './census/census.component';
+import { GnomeComponent } from './census/gnome/gnome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    CensusComponent,
+    GnomeComponent
   ],
   imports: [
     BrowserModule
